0bac5ffe27bcce2df6d8149dd24bc61f98d775f6
Anthony C. Jones <randomgeekdom@gmail.com>
2025-03-16 16:09:07 -0500

    Add project files.

---
+++ Colony.Core/Colony.cs
@@ -0,0 +1,25 @@
+﻿using Ardalis.Result;
+
+namespace Colony.Core
+{
+    public class Colony
+    {
+        private Colony(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; }
+        public uint Year { get; } = 0;
+
+        public static Result<Colony> TryCreate(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return Result.Error("Name is required");
+            }
+
+            return new Colony(name);
+        }
+    }
+}
